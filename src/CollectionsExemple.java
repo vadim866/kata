0f73fd9d5933e188d3494867e20f3b2dd0ba172d
li vadim <li.vadim86@gmail.com>
2023-09-03 13:23:54 +0900

    git

---
+++ src/CollectionsExemple.java
@@ -0,0 +1,18 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CollectionsExemple {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();//очередь интерфейс ее реализует линкедлист
+        //queue.add(1);//добавление
+        //queue.add(2);
+        //queue.add(3);
+        //while(!queue.isEmpty()) {//цикл вайл указываем что очередь не пустая
+            //System.out.println(queue.poll());//извлечение из очереди с помощью метода полл
+        //}
+        
+
+
+    }
+}
